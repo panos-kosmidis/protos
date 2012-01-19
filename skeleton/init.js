790480428c09418d37ea1ad18bfaee9e1bc8fea1
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-18 22:57:16 -0400

    Working on multi parallel exec

--- skeleton/init.js
+++ skeleton/init.js
@@ -2,6 +2,32 @@
 function Initialize(app) {
 
   app.debugMode = true;
+  app.asyncQueueTimeout = 0;
+  
+  var randFunc = function(callback) {
+    var t = Math.ceil(Math.random()*2000);
+    console.log('Started: ' + t);
+    setTimeout(function() {
+      callback.call(null, 'Finished: ' + t);
+    }, t);
+  }
+  
+  var context = {
+    method1: randFunc,
+    method2: randFunc,
+    method3: randFunc,
+    method4: randFunc
+  }
+  
+  var multi = new framework.Multi(context, {parallel: true});
+  
+  multi.method1();
+  multi.method2();
+  multi.method3();
+  multi.method4();
+  multi.exec(function(err, results) {
+    console.exit(err || results);
+  });
   
 }
 
