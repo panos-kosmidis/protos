fa8f1cc2e3b1b906db16bd29de8fce5bed676f94
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-18 22:05:58 -0400

    Cleanup

--- skeleton/init.js
+++ skeleton/init.js
@@ -3,14 +3,6 @@ function Initialize(app) {
 
   app.debugMode = true;
   
-  var sto = app.getResource('storages/redis');
-  
-  setTimeout(function() {
-    sto.get('name', function(err, value) {
-       console.exit(err || value);
-     });
-  }, 1000);
-  
 }
 
 module.exports = Initialize;
