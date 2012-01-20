6bb67355bc64ca908b585607ac339404086650b2
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-20 19:44:00 -0400

    Cleanup

--- skeleton/init.js
+++ skeleton/init.js
@@ -1,20 +1,6 @@
 
 function Initialize(app) {
 
-  app.debugMode = true;
-  
-  var store = app.getResource('storages/redis');
-  
-  var multi = store.multi();
-  
-  multi.get('name');
-  multi.get('name');
-  multi.get('hey');
-  multi.get('name');
-  
-  multi.exec(function(err, results) {
-    console.exit([err, results]);
-  });
 }
 
 module.exports = Initialize;
