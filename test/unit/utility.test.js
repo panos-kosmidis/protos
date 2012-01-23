f57b7cd03f426cc791e9007981266a97617ea575
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-23 19:34:09 -0400

    Update

--- test/unit/utility.test.js
+++ test/unit/utility.test.js
@@ -44,6 +44,50 @@ vows.describe('lib/utility.js').addBatch({
       assert.isFalse(framework.util.isTypeOf(99, 'function'));
     }
     
+  },
+  
+  'Utility::parseRange': {
+    
+    'Parses range strings': function() {
+      
+      function p(str) {
+        return framework.util.parseRange(1000, 'bytes='+str);
+      }
+      
+      assert.deepEqual(p('0-499'), [{start: 0, end: 499}]);
+      assert.deepEqual(p('40-80'), [{start: 40, end: 80}]);
+      assert.deepEqual(p('-500'), [{start: 500, end: 999}]);
+      assert.deepEqual(p('-400'), [{start: 600, end: 999}]);
+      assert.deepEqual(p('500-'), [{start: 500, end: 999}]);
+      assert.deepEqual(p('400-'), [{start: 400, end: 999}]);
+      assert.deepEqual(p('0-0'), [{start: 0, end: 0}]);
+      assert.deepEqual(p('-1'), [{start: 999, end: 999}]);
+    }
+    
+  },
+  
+  'Utility::searchPattern': {
+    
+    topic: function() {
+      return framework.util.searchPattern;
+    },
+    
+    'Detects single match w/ one find': function(f) {
+      assert.deepEqual(f('hello world!', 'world'), {world: [6]});
+    },
+    
+    'Detects single match w/ multiple finds': function(f) {
+      assert.deepEqual(f('hello world!', ['o']), {o: [4,7]});
+    },
+    
+    'Detects multiple matches w/ one find': function(f) {
+      assert.deepEqual(f('hello world', ['o', 'hello']), {o: [4,7], hello: [0]});
+    },
+    
+    'Detects multiple matches w/ multiple finds': function(f) {
+      assert.deepEqual(f('hello world', ['o', 'l']), {o: [4,7], l: [2,3,9]});
+    }
+    
   }
   
 }).export(module);
