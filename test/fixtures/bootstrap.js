abe24bc3624599c4c5cf5a3570561ae22e04f677
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-23 15:39:11 -0400

    Update

--- test/fixtures/bootstrap.js
+++ test/fixtures/bootstrap.js
@@ -9,6 +9,8 @@ var testSkeleton = CoreJS.path + '/test/fixtures/test-skeleton',
     framework = CoreJS.bootstrap(testSkeleton, {}),
     app = framework.defaultApp;
 
+app.logging = false;
+
 framework.path = CoreJS.path + '/test/fixtures/test-framework';
 
 module.exports = app;
