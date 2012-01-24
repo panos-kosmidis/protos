1583591508c99a25908efa5a9cb1671ef2736989
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-24 17:52:14 -0400

    Working on redis storage tests

--- test/special/storage.redis.js
+++ test/special/storage.redis.js
@@ -5,29 +5,83 @@ var app = require('../fixtures/bootstrap'),
     RedisClient = require('redis').RedisClient,
     EventEmitter = require('events').EventEmitter;
     
+var redis, multi;
+
+/*
+  Storage API Check Order:
+  
+  set
+  get
+  rename
+  setHash
+  getHash
+  updateHash
+  deleteFromHash
+  delete
+  expire
+
+*/
+
 vows.describe('lib/storages/redis.js').addBatch({
   
   'Integrity Checks': {
     
     topic: function() {
-      var promise = app.promise = new EventEmitter();
+      var promise = new EventEmitter();
       app.on('init', function() {
-        var redis = app.getResource('storages/redis');
-        promise.emit('success', redis);
+        redis = app.getResource('storages/redis');
+        multi = redis.multi({parallel: false, interrupt: false});
+        promise.emit('success');
       });
       return promise;
     },
 
-    'Sets db': function(redis) {
+    'Sets db': function() {
       assert.isNumber(redis.db);
     },
     
-    'Sets config': function(redis) {
-      assert.strictEqual(redis.config.host, app.config.storage.redis.host)
+    'Sets config': function() {
+      assert.strictEqual(redis.config.host, app.config.storage.redis.host);
     },
     
-    'Sets client': function(redis) {
+    'Sets client': function() {
       assert.instanceOf(redis.client, RedisClient);
+    }
+    
+  }
+  
+}).addBatch({
+  
+  /**
+    Inserts one or more records into the storage backend
+
+    Provides: [err]
+
+    Key can be either a string or an object containing key/value pairs
+
+    @param {string|object} key
+    @param {string} value (optional)
+    @param {function} callback
+   */  
+  
+  'RedisStorage::set': {
+    
+    topic: function() {
+      var promise = new EventEmitter()
+      multi.set('v1', 'Value 1');
+      multi.set({v2: 'Value 2', v3: 'Value 3'});
+      multi.exec(function(err, results) {
+        promise.emit('success', results);
+      });
+      return promise;
+    },
+    
+    'Sets a single value': function(results) {
+      assert.strictEqual(results[0], 'OK');
+    },
+    
+    'Sets multiple values': function(results) {
+      assert.strictEqual(results[1], 'OK');
     }
     
   }
