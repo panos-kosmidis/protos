fd85e613202670d1ce5be212d1494cb45e91a424
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-24 17:19:31 -0400

    Fixed port in lib/drivers/mysql.js

--- test/fixtures/bootstrap.js
+++ test/fixtures/bootstrap.js
@@ -12,20 +12,20 @@ if (module.parent.id == '.') {
   env = path.basename(path.dirname(module.parent.id));
 }
 
-console.exit(env);
-
 CoreJS.configure('autoCurl', false);
 
 CoreJS.on('app_init', function(app) {
-  app.config.database.mysql = testConfig.mysql;
-  app.config.storage.redis = testConfig.redis;
+  if (env == 'special' || env == 'integration') {
+    app.config.database.mysql = testConfig.mysql;
+    app.config.storage.redis = testConfig.redis;
+  }
 });
 
 var testSkeleton = CoreJS.path + '/test/fixtures/test-skeleton',
     framework = CoreJS.bootstrap(testSkeleton, {}),
     app = framework.defaultApp;
     
-app.logging = false;
+if (env == 'unit') app.logging = false;
 
 framework.path = CoreJS.path + '/test/fixtures/test-framework';
 
--- test/special/storage.redis.js
+++ test/special/storage.redis.js
@@ -1,18 +1,24 @@
 
 var app = require('../fixtures/bootstrap'),
     vows = require('vows'),
-    assert = require('assert');
+    assert = require('assert'),
+    EventEmitter = require('events').EventEmitter;
     
 vows.describe('lib/storages/redis.js').addBatch({
   
   'Integrity Checks': {
     
     topic: function() {
-      return app.getResource('storages/redis');
+      var promise = app.promise = new EventEmitter();
+      app.on('init', function() {
+        var redis = app.getResource('storages/redis');
+        promise.emit('success', redis);
+      });
+      return promise;
     },
-    
-    'Sets config': function(redis) {
-      assert.isObject(redis.config);
+
+    'Is true': function(redis) {
+      console.exit(redis.get);
     }
     
   }
