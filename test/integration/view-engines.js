fb980b565449a6696c95f9334723bf0825e9c2be
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-26 12:10:48 -0400

    Added fixtures + extra tests

---
+++ test/integration/view-engines.js
@@ -0,0 +1,16 @@
+
+var app = require('../fixtures/bootstrap'),
+    vows = require('vows'),
+    assert = require('assert'),
+    util = require('util'),
+    EventEmitter = require('events').EventEmitter;
+    
+vows.describe('View Engines').addBatch({
+  
+  'Eco': {
+    
+    
+    
+  }
+  
+}).export(module);
