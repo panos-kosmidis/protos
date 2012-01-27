644dd68d7b0085ab4cc7e128c95b3c6d5a7b3ada
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-27 11:48:47 -0400

    Working on handlebars partials fix

--- test/fixtures/bootstrap.js
+++ test/fixtures/bootstrap.js
@@ -79,7 +79,7 @@ app.__createEngineBatch = function(className, testUrl, __module__) {
       topic: function() {
         var promise = new EventEmitter();
         app.clientRequest(testUrl  , function(err, buffer, headers) {
-          console.exit(buffer);
+          // console.exit(buffer);
           promise.emit('success', err || buffer);
         });
         return promise;
