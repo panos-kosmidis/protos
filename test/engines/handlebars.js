95e14dcde7a5504cedb0469f5f8e9e8b36911c9f
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-26 23:10:13 -0400

    Update

---
+++ test/engines/handlebars.js
@@ -0,0 +1,10 @@
+
+var app = require('../fixtures/bootstrap');
+
+app.addFilter('handlebars_template', function(data) {
+  data = app.__addEnginePartials('handlebars', data, '');
+  console.exit(data);
+  return data;
+});
+
+app.__createEngineBatch('Handlebars', '/handlebars.hb.html', module);
