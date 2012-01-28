8a8862090be04ad2039dd82db070cd14e3d53be7
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-28 12:07:32 -0400

    Added view caching tests

--- test/integration/view-cache.js
+++ test/integration/view-cache.js
@@ -5,18 +5,20 @@ var app = require('../fixtures/bootstrap'),
     assert = require('assert'),
     OutgoingMessage = require('http').OutgoingMessage;
 
-var enabledBatch = 'When View Caching is enabled',
-    disabledBatch = 'When View Caching is disabled',
+var enabledBatch = 'When Enabled',
+    disabledBatch = 'When Disabled',
     batch = {};
     
 var current = batch[enabledBatch] = {};
 batch[disabledBatch] = {};
 
+// Prevent conflicts
+var oldViewCache = app.views.callbacks;
+app.views.callbacks = {};
+
 // ViewCaching enabled
 app.viewCaching = true;
-var counter = 0, 
-    results = [],
-    viewCache = app.views.callbacks;
+var counter = 0;
 
 Object.keys(app.engines).map(function(eng) {
   var engine = app.engines[eng],
@@ -24,13 +26,37 @@ Object.keys(app.engines).map(function(eng) {
   res.engine = engine;
   current[util.format('%s engine caches callbacks', engine.className)] = function() {
     var relPath = '/myview/'+ (++counter);
-        out = engine.render('SUCCESS', {res: res}, relPath);
-    results.push(out);
-    var cb = viewCache[relPath];
+    engine.render('', {res: res}, relPath);
+    var cb = app.views.callbacks[relPath];
     assert.isFunction(cb);
-    assert.isString(cb.engine);
     assert.equal(cb.engine, engine.className);
   }
 });
+
+current['Reset view cache state'] = function() {
+  app.viewCaching = false;
+  counter = 0;
+  app.views.callbacks = {};
+}
+
+// ViewCaching disabled
+current = batch[disabledBatch];
+Object.keys(app.engines).map(function(eng) {
+  var engine = app.engines[eng],
+      res = new OutgoingMessage;
+  res.engine = engine;
+  current[util.format('%s engine does not cache callbacks', engine.className)] = function() {
+    var relPath = '/myview/'+ (++counter);
+    engine.render('HELLO', {res: res}, relPath);
+    var cb = app.views.callbacks[relPath];
+    assert.isUndefined(cb);
+  }
+});
+
+current['Restored view cache state'] = function() {
+  app.views.callbacks = oldViewCache;
+}
+
+// last check
 
 vows.describe('View Caching').addBatch(batch).export(module);
