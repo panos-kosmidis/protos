326367ee998867eba80affd982a127dde7b33e12
Ernesto Mendez <der.kunstler@gmail.com>
2012-02-12 17:34:35 -0400

    Additional bootstrap enhancements. Closes #63 - Ability to run environment-specific code directly from bootstrap - Ability to enable middleware from bootstrap

--- skeleton/boot.js
+++ skeleton/boot.js
@@ -2,11 +2,41 @@
 var CoreJS = require('../');
 
 CoreJS.bootstrap(__dirname, {
+  
+  /* Host Configuration */
   host: 'localhost',
   port: 8080,
-  environment: 'development',
   multiProcess: false,
   stayUp: false,
+  
+  /* Automatic Redirection */
   redirect: false,
-  events: {}
+  
+  /* Environment Configuration */
+  environment: {
+    
+    /* Default Environment */
+    default: 'development',
+    
+    /* Production Environment code */
+    production: function(app) {
+      // Use port 80 on production
+      app.use('production_port');
+    }
+
+  },
+  
+  /* Event Hooks */
+  events: {
+    // init: function(app) {
+    //   app.log('Application initialized!');
+    // }
+  },
+  
+  /* Load Middleware */
+  middleware: {
+    // session: {storage: 'redis', guestSessions: true};
+    // response_cache: {storage: 'redis'};
+  }
+  
 });
