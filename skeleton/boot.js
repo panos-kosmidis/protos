e7c2b9cbd6201e61f5e9aeeb53eff7abf6fd0e5e
Ernesto Mendez <der.kunstler@gmail.com>
2012-02-12 17:40:25 -0400

    Cleanup [ci skip]

--- skeleton/boot.js
+++ skeleton/boot.js
@@ -3,40 +3,24 @@ var CoreJS = require('../');
 
 CoreJS.bootstrap(__dirname, {
   
-  /* Host Configuration */
   host: 'localhost',
   port: 8080,
   multiProcess: false,
   stayUp: false,
-  
-  /* Automatic Redirection */
   redirect: false,
   
-  /* Environment Configuration */
   environment: {
-    
-    /* Default Environment */
     default: 'development',
-    
-    /* Production Environment code */
     production: function(app) {
       // Use port 80 on production
       app.use('production_port');
     }
-
   },
   
-  /* Event Hooks */
-  events: {
-    // init: function(app) {
-    //   app.log('Application initialized!');
-    // }
-  },
+  // Attach events
+  events: {},
   
-  /* Load Middleware */
-  middleware: {
-    // session: {storage: 'redis', guestSessions: true};
-    // response_cache: {storage: 'redis'};
-  }
+  // Load middleware
+  middleware: {}
   
 });
