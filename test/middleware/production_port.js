b1feeea43a38608a77661e013d585eb1ad95d6dc
Ernesto Mendez <der.kunstler@gmail.com>
2012-02-12 20:37:12 -0400

    Refactored Application::use - Added production_port middleware test case

---
+++ test/middleware/production_port.js
@@ -0,0 +1,15 @@
+
+var app = require('../fixtures/bootstrap'),
+    vows = require('vows'),
+    assert = require('assert');
+    
+vows.describe('Production Port (middleware)').addBatch({
+  
+  "Removes port from app.baseUrl": function() {
+    var oldUrl = app.baseUrl;
+    app.use('production_port');
+    assert.equal(app.baseUrl, 'http://' + app.domain);
+    app.baseUrl = oldUrl;
+  }
+  
+}).export(module);
