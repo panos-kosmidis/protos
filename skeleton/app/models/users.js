05c5cdf3336942fc4334b49ccaf69770392c8af5
Ernesto Mendez <der.kunstler@gmail.com>
2012-02-29 11:27:22 -0400

    Refactoring

--- skeleton/app/models/users.js
+++ skeleton/app/models/users.js
@@ -4,9 +4,11 @@ function UsersModel(app) {
   this.driver = 'mongodb';
   
   this.properties = {
-    user  : {type: 'string', unique: true, required: true, validates: 'alnum_underscores'},
-    pass  : {type: 'string', required: true, validates: 'password'},
-    date  : {type: 'timestamp', validates: 'timestamp', default: function() { return new Date(); }},
+    user    : {type: 'string', unique: true, required: true, validates: 'alnum_underscores'},
+    pass    : {type: 'string', required: true, validates: 'password'},
+    date    : {type: 'timestamp', validates: 'timestamp', default: function() { return new Date(); }},
+    object  : {type: 'object', default: {a: 1, b: 2, c: 3}},
+    array   : {type: 'array', default: [1,2,3,4]}
   }
   
   this.hasOne = ['company', 'website'];
--- skeleton/init.js
+++ skeleton/init.js
@@ -1,11 +1,24 @@
 
 function Initialize(app) {
 
-  app.usersModel.create({
-    user: 'ernie',
-    pass: 'passme'
-  }, function(err, model) {
-    console.exit(err || model);
+  // app.usersModel.create({
+  //   user: 'ernie',
+  //   pass: 'passme',
+  // }, function(err, model) {
+  //   console.exit(err || model);
+  // });
+  
+  app.usersModel.get('4f4e33b1c4b032a833000001', function(err, model) {
+    if (err) throw err;
+    else {
+      
+      model.array.push(99);
+      
+      model.save(function(err) {
+        console.exit(err);
+      });
+      
+    }
   });
 
 }
