8aef80e49d880af8d75c62440ea7c5a710391df1
Ernesto Mendez <der.kunstler@gmail.com>
2012-02-29 12:10:58 -0400

    Update

--- skeleton/config/database.js
+++ skeleton/config/database.js
@@ -9,6 +9,14 @@ module.exports = {
    host: 'localhost',
    port: 27017,
    database: 'myapp'
+  },
+  
+  mysql: {
+    host: 'localhost',
+    port: 3306,
+    user: 'db_user',
+    password: 'db_pass',
+    database: 'test_db'
   }
 
 }
--- skeleton/init.js
+++ skeleton/init.js
@@ -8,23 +8,23 @@ function Initialize(app) {
   //   console.exit(err || model);
   // });
   
-  // app.usersModel.get('4f4e49cc8eac87143c000001', function(err, model) {
-  //   if (err) throw err;
-  //   else {
-  //     
-  //     // console.exit(model);
-  //     
-  //     model.valid = ! model.valid;
-  //     model.friends++;
-  //     model.array.push(99);
-  //     model.object.a = 11;
-  //     
-  //     model.save(function(err) {
-  //       console.exit(err);
-  //     });
-  //     
-  //   }
-  // });
+  app.usersModel.get(1, function(err, model) {
+    if (err) throw err;
+    else {
+      
+      console.exit(model);
+      
+      // model.valid = ! model.valid;
+      // model.friends++;
+      // model.array.push(99);
+      // model.object.a = 11;
+      // 
+      // model.save(function(err) {
+      //   console.exit(err);
+      // });
+      
+    }
+  });
 
 }
 
