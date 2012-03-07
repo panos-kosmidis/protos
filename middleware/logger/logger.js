7b32e55b512abdf1b127eea012ccee0e93409e1b
Ernesto Mendez <der.kunstler@gmail.com>
2012-03-07 15:52:58 -0400

    Added `detailed` logging format for logger middleware

--- middleware/logger/logger.js
+++ middleware/logger/logger.js
@@ -103,7 +103,7 @@ function Logger(config, middleware) {
   this.config = config = corejs.extend({
     accessLog: true,
     accessLogConsole: true,
-    accessLogFormat: null,
+    accessLogFormat: 'default',
     infoLevel: {file: 'info.log', console: true},
     errorLevel: {file: 'error.log', console: true}
   }, config);
@@ -122,16 +122,20 @@ Logger.prototype.enableAccessLog = function() {
   var self = this,
       config = this.config,
       accessLogFormat = this.config.accessLogFormat;
-  
-  if (!accessLogFormat) config.accessLogFormat = accessLogFormat = function(req, res, app) {
-    var ms = Date.now() - req.startTime;
-    return util.format('%s (%s) [%s] %s %s %s (%s)', app.domain, app.date(), req.socket.remoteAddress, req.method, 
-    req.url, res.statusCode, self.timeDelta(ms));
-  };
+      
+  if (typeof accessLogFormat == 'string') {
+    if (accessLogFormat in this.logFormats) {
+      accessLogFormat = this.logFormats[accessLogFormat];
+    } else {
+      throw new Error("Logger: Unknown log format: " + accessLogFormat);
+    }
+  } else if (typeof accessLogFormat != 'function') {
+    throw new Error("Logger: accessLogFormat accepts either a string or a function: " + accessLogFormat);
+  }
   
   // Access log filter
   var accessLogFilter = function() {
-    var log = accessLogFormat(this.request, this, app);
+    var log = accessLogFormat.call(self, this.request, this, app);
     app.emit('access_log', log);
     if (config.accessLogConsole) console.log(log);
     if (config.accessLog) accessLog.write(log+'\n');
@@ -141,6 +145,19 @@ Logger.prototype.enableAccessLog = function() {
   app.on('request', function(req, res) {
     res.on('finish', accessLogFilter);
   });
+}
+
+Logger.prototype.logFormats = {
+  default: function(req, res, app) {
+    var ms = Date.now() - req.startTime;
+    return util.format('%s (%s) [%s] %s %s %s (%s)', app.domain, app.date(), req.socket.remoteAddress, req.method, 
+    req.url, res.statusCode, this.timeDelta(ms));
+  },
+  detailed: function(req, res, app) {
+    var ms = Date.now() - req.startTime;
+    return util.format('%s (%s) [%s] %s %s %s (%s) - %s', app.domain, app.date(), req.socket.remoteAddress, req.method, 
+    req.url, res.statusCode, this.timeDelta(ms), req.headers['user-agent']);
+  }
 }
 
 Logger.prototype.timeDelta = function(ms) {
