72123a46439b90eb5013204d3c18d30292ad36a4
derdesign <der.kunstler@gmail.com>
2012-06-23 23:52:55 -0400

    Fix db config in travis env

--- test/fixtures/test.skeleton/config/env/travis.js
+++ test/fixtures/test.skeleton/config/env/travis.js
@@ -3,18 +3,22 @@
 
 function Travis(app) {
   
-  // http://about.travis-ci.org/docs/user/database-setup/
-  
-  var mysql = app.config.database.mysql.nocache,
-      mysqlc = app.config.database.mysql.cache;
-  
-  // Override mysql configuration on travis
-  
-  mysql.host = mysqlc.host = '0.0.0.0';
-  mysql.user = mysqlc.user = 'root';
-  mysql.password = mysqlc.password = '';
-  
-  // Note: Redis uses default settings, no need to configure
+  app.on('pre_init', function() {
+    
+    // http://about.travis-ci.org/docs/user/database-setup/
+
+    var mysql = app.config.database.mysql.nocache,
+        mysqlc = app.config.database.mysql.cache;
+
+    // Override mysql configuration on travis
+
+    mysql.host = mysqlc.host = '0.0.0.0';
+    mysql.user = mysqlc.user = 'root';
+    mysql.password = mysqlc.password = '';
+
+    // Note: Redis uses default settings, no need to configure
+    
+  });
   
 }
 
