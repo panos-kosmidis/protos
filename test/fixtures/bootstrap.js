8c69ddbe4be34c5877a42be5caeefaac68070343
Panagiotis Kosmidis <panos@hostindex.gr>
2012-07-16 17:06:16 +0300

    JinJS & Kiwi tests
    Added JinJS and Kiwi engines in bootstrap tests

--- test/fixtures/bootstrap.js
+++ test/fixtures/bootstrap.js
@@ -53,7 +53,7 @@ var protos = Protos.bootstrap(testSkeleton, {
           protos.loadDrivers('mongodb', 'mysql');
           protos.loadStorages('mongodb', 'redis');
           protos.loadEngines('coffeekup', 'dot', 'eco', 'ejs', 'haml', 'hamlcoffee', 'handlebars', 'hogan', 'jade',
-          'jazz', 'jqtpl', 'jshtml', 'kernel', 'liquor', 'swig', 'whiskers');
+          'jazz', 'jinjs', 'jqtpl', 'jshtml', 'kernel', 'kiwi', 'liquor', 'swig', 'whiskers');
         },
         pre_init: function(app) {
           // Test skeleton properties
