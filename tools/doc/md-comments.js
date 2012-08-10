46481ca4f33823edb0de87c1c3d6f21322edc700
derdesign <der.kunstler@gmail.com>
2012-08-09 22:17:42 -0400

    Using marked to parse markdown in comments

--- tools/doc/md-comments.js
+++ tools/doc/md-comments.js
@@ -4,9 +4,15 @@
 
 var util = require('util'),
     underscore = require('underscore'),
-    discount = require('discount'),
+    markdown = require('marked'),
     fs = require('fs'),
     pathModule = require('path');
+    
+markdown.setOptions({
+  gfm: true,
+  sanitize: false,
+  pedantic: false
+});
 
 var tagRegex = /^(\s+)?@(.*?)$/mg;
 
@@ -14,10 +20,6 @@ var tagRegex = /^(\s+)?@(.*?)$/mg;
 console.exit = function() { 
   console.log(util.format.apply(null, arguments)); 
   process.exit(); 
-}
-
-function markdown(str) {
-  return discount.parse(str, discount.flags.autolink);
 }
 
 function searchPattern(buffer, s) {
